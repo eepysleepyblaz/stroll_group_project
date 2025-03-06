460d38eec3dff82006a58be38d47552fcd91d850
Sandy Emmerson <2893523e@student.gla.ac.uk>
2025-03-06 17:34:28 +0000

    Simple content added to the base template

--- stroll/views.py
+++ stroll/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 def home(request):
+    # Variables needed for home template:
+    # Array of popular walks
+    # Current code just for test
     context_dict = {}
-    context_dict["popular_walks"] = ["Walk 1", "Walk 2", "Walk 3", "Walk 4", "Walk 5"]
+    context_dict["popular_walks"] = [{"thumbnail": "a photo", "name": "my first walk", "area": "partick", "tags": "hi,hello,good".split(","), 
+                                      "difficulty": 1, "description": "Thuis is my really cool walk"},
+                                      {"thumbnail": "another photo", "name": "my last walk", "area": "govan", "tags": "goodbye,fairwell,bad".split(","), 
+                                      "difficulty": 4, "description": "Thuis is my really bad walk"},
+                                      {"thumbnail": "a picture", "name": "my new walk", "area": "leith", "tags": "i,regret,this".split(","), 
+                                      "difficulty": 10, "description": "Thuis is not fun"}]
     return render(request, 'stroll/home.html', context=context_dict)
 
 def about(request):
