ce7553d0b1063779acb36e23a5d180c53a6ae15c
Brad Ciohodaru <2685000c@student.gla.ac.uk>
2025-03-08 17:16:58 +0000

    Created model for user

--- stroll/models.py
+++ stroll/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length = 30)
+    date_of_birth = models.DateField()
+    description = models.CharField(max_length = 500)
+    password = models.CharField(max_length = 12)
+    is_moderator = models.BooleanField()
+    total_likes = models.IntegerField()
+    total_views = models.IntegerField()
