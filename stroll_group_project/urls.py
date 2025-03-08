d1d938ea35e9a7eada89cffa2107e6e7f28cd641
2685000c <2685000c@student.gla.ac.uk>
2025-03-08 18:35:12 +0000

    Added the media folder and subfolders. Also made the changes necessary for it to work

--- stroll_group_project/urls.py
+++ stroll_group_project/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from stroll import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('stroll/', include('stroll.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
