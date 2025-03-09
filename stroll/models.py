73f4fd37bcdbcac384b337c56e0d0fe09fc9952c
Brad Ciohodaru <2685000c@student.gla.ac.uk>
2025-03-09 16:46:06 +0000

    Added __str__ method to models

--- stroll/models.py
+++ stroll/models.py
@@ -9,6 +9,11 @@ class User(models.Model):
     total_likes = models.IntegerField(default=0)
     total_views = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.username
+
+
+
 class Walk(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) # Foreign key
     title = models.CharField(max_length=30)
@@ -26,11 +31,21 @@ class Walk(models.Model):
     gallery_image_4 = models.ImageField(upload_to='gallery/')
     map_coordinates = models.CharField(max_length=5000)
 
+    def __str__(self):
+        return self.title
+    
+
+
 class Question(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) # Foreign key
     date_published = models.DateField.auto_now_add
     title = models.CharField(max_length=100)
     text = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
+
+
 
 class WalkComment(models.Model):
     walk = models.ForeignKey(Walk, on_delete=models.CASCADE) # Foreign key
@@ -38,8 +53,16 @@ class WalkComment(models.Model):
     date_published = models.DateField.auto_now_add
     text = models.CharField(max_length=300)
 
+    def __str__(self):
+        return self.user.username + " comment on walk with title: " + self.walk.title
+
+
+
 class QuestionComment(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) # Foreign key
     user = models.ForeignKey(User, on_delete=models.CASCADE) # Foreign key
     date_published = models.DateField.auto_now_add
     text = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.user.username + " comment on question with title: " + self.question.title
