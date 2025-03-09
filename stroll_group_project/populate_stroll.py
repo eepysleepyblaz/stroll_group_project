57db536ed577ee4df8746affd678cb618835add2
Brad Ciohodaru <2685000c@student.gla.ac.uk>
2025-03-09 19:04:22 +0000

    Made changes to population script to account for foreign keys

--- stroll_group_project/populate_stroll.py
+++ stroll_group_project/populate_stroll.py
@@ -15,7 +15,8 @@ def populate():
         'password':'1234',
         'is_moderator':False,
         'walks':walks[2],
-        'questions':questions[1]},
+        'questions':questions[1],
+        'comments':[walk_comments[0],question_comments[2]]},
 
         {'username':'kevin2',
         'date_of_birth':'1982-03-09',
@@ -23,7 +24,8 @@ def populate():
         'password':'12345',
         'is_moderator':False,        
         'walks':walks[1],
-        'questions':questions[2]},
+        'questions':questions[2],
+        'comments':[walk_comments[2],question_comments[0]]},
 
         {'username':'scott3',
         'date_of_birth':'1970-05-03',
@@ -31,12 +33,13 @@ def populate():
         'password':'123456',
         'is_moderator':True,
         'walks':walks[0],
-        'questions':questions[0]},
+        'questions':questions[0],
+        'comments':[walk_comments[1],question_comments[1]]},
     ]
     
     walks = [
         {'title':'Govan walk',
-        'description':'Walking around govan, took photos of the river'},
+        'description':'Walking around govan, took photos of the river',},
 
         {'title':'Milngaive walk',
         'description':'The neighbourhood seems nice'},
@@ -59,7 +62,7 @@ def populate():
     walk_comments = [
         {'text':'nice walk'},
 
-        {'text':'There is another way you can take, has some cool shops.'},
+        {'text':'There is another way you can take, has some interesting shops.'},
 
         {'text':"try going to glasgow green, it's not too far"},
     ]
@@ -72,31 +75,43 @@ def populate():
         {'text':'here: *link to walk in nature*'},
     ]
 
-    def add_user(username, date_of_birth, description, password, is_moderator):
-        u = User.objects.get_or_create(username=username, date_of_birth=date_of_birth)[0]
-        u.description = description
-        u.password = password
-        u.is_moderator = is_moderator
-        u.save()
-        return u
+    for user in users:
+        u = add_user(user['username'], user['date_of_birth'],
+                     user['description'], user['password'],
+                     user['is_moderator'])
+        
+        for walk in user['walks']:
+            pass
+
+def add_user(username, date_of_birth, description, password, is_moderator):
+    u = User.objects.get_or_create(username=username, date_of_birth=date_of_birth)[0]
+    u.description = description
+    u.password = password
+    u.is_moderator = is_moderator
+    u.save()
+    return u
+
+def add_walk(user, title, description):
+    w = Walk.objects.get_or_create(title=title, description=description)[0]
+    w.save()
+    return w
+
+def add_question(user, title, text):
+    q = Question.objects.get_or_create(title=title, text=text)[0]
+    q.save()
+    return q
+
+def add_walk_comment(user, walk, text):
+    wc = WalkComment.objects.get_or_create(text=text)[0]
+    wc.save()
+    return wc
+
+def add_question_comment(user, question, text):
+    qc = QuestionComment.objects.get_or_create(text=text)[0]
+    qc.save()
+    return qc
     
-    def add_walk(title, description):
-        w = Walk.objects.get_or_create(title=title, description=description)[0]
-        w.save()
-        return w
-    
-    def add_question(title, text):
-        q = Question.objects.get_or_create(title=title, text=text)[0]
-        q.save()
-        return q
-    
-    def add_walk_comment(text):
-        wc = WalkComment.objects.get_or_create(text=text)[0]
-        wc.save()
-        return wc
-    
-    def add_question_comment(text):
-        qc = QuestionComment.objects.get_or_create(text=text)[0]
-        qc.save()
-        return qc
-    
+
+if __name__ == '__main__':
+    print('Starting Stroll population script...')
+    populate()
