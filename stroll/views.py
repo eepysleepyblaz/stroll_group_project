0e6b8634fa0e67815c5fa9b9569322925e1c46d1
Sandy Emmerson <2893523e@student.gla.ac.uk>
2025-03-18 15:45:54 +0000

    Search walks html done needs the jscript and ajax

--- stroll/views.py
+++ stroll/views.py
@@ -48,7 +48,15 @@ def my_questions(request):
     return render(request, 'stroll/edit_profile.html')
 
 def search_walks(request):
-    return render(request, 'stroll/search_walks.html')
+    context_dict = {}
+    context_dict["search_results"] = [{"thumbnail": "walk_hill", "name": "my first walk", "area": "partickwwwwwwwwwwwwwwww", "tags": "hi,hello,good".split(","), 
+                                      "difficulty": 1, "description": "Thuis is my really cool walkssssssssssssssss ssssssssssssssssssssssss ssssssssssssssssssssssssssssssssssss sssssssssssssssssssssssssssss sssssssssssssssssssssssssssss sssssssssssssssssssssssssssssssssssssssssssssssssssssssssss", "slug": "a"},
+                                      {"thumbnail": "walk_hill", "name": "my last walk", "area": "govan", "tags": "goodbye,fairwell,bad".split(","), 
+                                      "difficulty": 4, "description": "Thuis is my really bad walk", "slug": "a"},
+                                      {"thumbnail": "walk_hill", "name": "my new walk", "area": "leith", "tags": "i,regret,this".split(","), 
+                                      "difficulty": 10, "description": "Thuis is not fun", "slug": "a"},
+                                      ]
+    return render(request, 'stroll/search_walks.html', context=context_dict)
 
 def show_walk(request, walk_slug):
     return render(request, 'stroll/walk.html')
