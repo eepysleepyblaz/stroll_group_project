a7317fe85e412ccaec56bb183e501ccb3b835e02
Sandy Emmerson <2893523e@student.gla.ac.uk>
2025-03-20 16:41:12 +0000

    Walk page ready of js and models and user athentication

--- stroll/views.py
+++ stroll/views.py
@@ -80,8 +80,10 @@ def search_walks(request):
     return render(request, 'stroll/search_walks.html', context=context_dict)
 
 def show_walk(request, walk_name_slug):
+    
     context_dict = {"user": "emi", "thumbnail": "walk_hill", "photo1": "photo", "name": "my first walk", "length": "100km", "area": "partickwwwwwwwwwwwwwwww", "tags": "hi,hello,good".split(","), 
                                       "difficulty": 1, "description": "Thuis is my really cool walkssssssssssssssss ssssssssssssssssssssssss ssssssssssssssssssssssssssssssssssss sssssssssssssssssssssssssssss sssssssssssssssssssssssssssss sssssssssssssssssssssssssssssssssssssssssssssssssssssssssss", "slug": "a"}
+    context_dict['comments'] = [{"text": "Good job", "date_published": "13/20/24", "user": "jules", "profile_picture": "photo"}]
     return render(request, 'stroll/walk.html', context=context_dict)
 
 def questions(request):
