903c4b3c73a9e55b459dca27fe1bc474cb1651f0
Sandy Emmerson <2893523e@student.gla.ac.uk>
2025-03-21 14:33:18 +0000

    Made the user authentication visable everywhere

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # stroll_group_project
+
+# External Sources:
+# Bootstrap V5 - Used to streamline the CSS
+# https://getbootstrap.com/docs/5.0/getting-started/introduction/
--- stroll/templatetags/stroll_template_tags.py
+++ stroll/templatetags/stroll_template_tags.py
@@ -2,14 +2,23 @@ from django import template
 
 register = template.Library()
 
+# Creates a walk from its attributes
 @register.inclusion_tag('stroll/walk_element.html')
 def walk_element(walk, display_type):
     return {"walk": walk, "display_type": display_type}
 
+# Creates a question from its attributes
 @register.inclusion_tag('stroll/question_element.html')
 def question_element(question):
     return {"question": question}
 
+# Creates a comment from its attributes
 @register.inclusion_tag('stroll/comment_element.html')
 def comment_element(comment):
     return {"comment": comment}
+
+# Creates a comment section from an array some comments
+# User need to passed into this
+@register.inclusion_tag('stroll/comments_section.html')
+def comments_section(comments, user):
+    return {"comments": comments, "user": user}
