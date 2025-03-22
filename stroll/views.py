399c21f9c1a7127069897fae305c45582e04fe34
2685000c <2685000c@student.gla.ac.uk>
2025-03-22 17:33:32 +0000

    Fixed superuser error in my_profile view, also made progress on using the database to get information on a walk

--- stroll/views.py
+++ stroll/views.py
@@ -115,32 +115,63 @@ def user_logout(request):
 @login_required
 def my_profile(request):
     user = request.user
+
     try:
         current_user_profile = UserProfile.objects.get(user=user)
     except UserProfile.DoesNotExist:
         current_user_profile = None
 
     walks = Walk.objects.filter(user=current_user_profile).order_by('-date_published')
+    walks_list = []
+    if walks != None:
+        for walk in walks:
+            walk_information = {}
+            walk_information['thumbnail'] = walk.thumbnail
+            walk_information['area'] = walk.area
+            walk_information['tags'] = walk.tags.split(',') # Must be formatted: 'tag1,tag2,tag3...'
+            walk_information['difficulty'] = str(walk.difficulty)
+            walk_information['desciption'] = walk.description
+            walk_information['slug'] = 'a'
+            walk_information.append(walks_list)
 
-    if current_user_profile == None:
-        return render(request, 'Invalid user')
+    if user.is_superuser:
+        context_dict = {}
+        context_dict['username'] = user.username + ' (superuser)'
+
+    elif current_user_profile == None:
+        context_dict = {}
+        context_dict['username'] = 'None type user'
+    
     else:
         context_dict = {}
-        context_dict['username'] = current_user_profile.user.username
+        if current_user_profile.is_moderator:
+            context_dict['username'] = current_user_profile.user.username + ' (moderator)'
+        else:
+            context_dict['username'] = current_user_profile.user.username
+
         context_dict['email'] = current_user_profile.user.email
         context_dict['date_of_birth'] = current_user_profile.date_of_birth
         context_dict['profile_picture'] = current_user_profile.profile_picture 
         context_dict['total_likes'] = current_user_profile.total_likes 
         context_dict['total_views'] = current_user_profile.total_views 
-        context_dict['recent_walks'] = [{"thumbnail": "walk_hill", "name": "my first walk", "area": "partickwwwwwwwwwwwwwwww", "tags": "hi,hello,good".split(","), 
-                                      "difficulty": 1, "description": "Thuis is my really cool walkssssssssssssssss ssssssssssssssssssssssss ssssssssssssssssssssssssssssssssssss sssssssssssssssssssssssssssss sssssssssssssssssssssssssssss sssssssssssssssssssssssssssssssssssssssssssssssssssssssssss", "slug": "a"},
-                                      {"thumbnail": "walk_hill", "name": "my last walk", "area": "govan", "tags": "goodbye,fairwell,bad".split(","), 
-                                      "difficulty": 4, "description": "Thuis is my really bad walk", "slug": "a"},
-                                      {"thumbnail": "walk_hill", "name": "my new walk", "area": "leith", "tags": "i,regret,this".split(","), 
-                                      "difficulty": 10, "description": "Thuis is not fun", "slug": "a"},
-                                      ]
+
+        if len(walks_list) != 0:
+            context_dict['recent_walks'] = walks_list
+        else:
+            context_dict['recent_walks'] = None
+
+
+
+
+        # context_dict['recent_walks'] = [{"thumbnail": "walk_hill", "name": "my first walk", "area": "partickwwwwwwwwwwwwwwww", "tags": "hi,hello,good".split(","), 
+        #                               "difficulty": 1, "description": "Thuis is my really cool walkssssssssssssssss ssssssssssssssssssssssss ssssssssssssssssssssssssssssssssssss sssssssssssssssssssssssssssss sssssssssssssssssssssssssssss sssssssssssssssssssssssssssssssssssssssssssssssssssssssssss", "slug": "a"},
+        #                               {"thumbnail": "walk_hill", "name": "my last walk", "area": "govan", "tags": "goodbye,fairwell,bad".split(","), 
+        #                               "difficulty": 4, "description": "Thuis is my really bad walk", "slug": "a"},
+        #                               {"thumbnail": "walk_hill", "name": "my new walk", "area": "leith", "tags": "i,regret,this".split(","), 
+        #                               "difficulty": 10, "description": "Thuis is not fun", "slug": "a"},
+        #                               ]
             
-        return render(request, 'stroll/my_profile.html', context=context_dict)
+    return render(request, 'stroll/my_profile.html', context=context_dict)
 
 def edit_profile(request):
     return render(request, 'stroll/edit_profile.html')
