565e04452f53a39ac92651fd16d04468145c89f0
Tom Craig <2890931c@student.gla.ac.uk>
2025-03-23 12:25:02 +0000

    Only Show Filled Gallery Images

--- stroll/views.py
+++ stroll/views.py
@@ -14,13 +14,7 @@ def home(request):
     # Current code just for test
     context_dict = {}
     context_dict["popular_walks"] = walks = Walk.objects.order_by('-likes')[:3]
-    """[{"thumbnail": "walk_hill", "name": "my first walk", "area": "partickwwwwwwwwwwwwwwww", "tags": "hi,hello,good".split(","), 
-                                      "difficulty": 1, "description": "Thuis is my really cool walkssssssssssssssss ssssssssssssssssssssssss ssssssssssssssssssssssssssssssssssss sssssssssssssssssssssssssssss sssssssssssssssssssssssssssss sssssssssssssssssssssssssssssssssssssssssssssssssssssssssss", "slug": "a"},
-                                      {"thumbnail": "walk_hill", "name": "my last walk", "area": "govan", "tags": "goodbye,fairwell,bad".split(","), 
-                                      "difficulty": 4, "description": "Thuis is my really bad walk", "slug": "a"},
-                                      {"thumbnail": "walk_hill", "name": "my new walk", "area": "leith", "tags": "i,regret,this".split(","), 
-                                      "difficulty": 10, "description": "Thuis is not fun", "slug": "a"},
-                                      ]"""
+
     return render(request, 'stroll/home.html', context=context_dict)
 
 def about(request):
@@ -186,7 +180,7 @@ def show_walk(request, id):
     walk = Walk.objects.get(id=id)
 
     context_dict = {'walk':walk}
-    context_dict['photos'] = [walk.gallery_image_1, walk.gallery_image_2, walk.gallery_image_3, walk.gallery_image_4]
+    context_dict['photos'] = [x for x in [walk.gallery_image_1, walk.gallery_image_2, walk.gallery_image_3, walk.gallery_image_4] if x != ""]
     context_dict['map_coordinates'] = walk.map_coordinates
     
     #context_dict = {"user": "emi", "thumbnail": "walk_hill", "photo1": "photo", "name": "my first walk", "length": "100km", "area": "partickwwwwwwwwwwwwwwww", "tags": "hi,hello,good".split(","), 
